1223a57a8452f3328735a9651d0fd6c1ae7e3f72
baris5534 <samsuncrsmb55@gmail.com>
2023-12-04 16:20:57 +0300

    2

---
+++ src/components/CalculatorApp.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="grid grid-cols-4 w-72 gap-2 text-xl mx-auto font-bold bg-cyan-900 p-3 my-auto">
+        <span class="col-span-3 text-white flex">Calculator</span>
+        <div class="col-span-4 bg-slate-400 p-5 text-end">{{ calculatorValue || 0 }}</div>
+        <div v-for="n in calculatorElements" :key="n">
+            <div class="text-white text-center bg-orange-600 p-5" 
+            :class="{'bg-vue-green':['C','*','/','-','+','%','='].includes(n)}" @click="action(n)">
+                {{ n }}
+            </div>
+        </div>
+        <!-- <button class="bg-orange-400 p-5 text-red-800">C</button>
+        <button class="bg-orange-400 p-5">%</button>
+        <button class="bg-orange-400 p-5">()</button>
+        <button class="bg-orange-400 p-5">/</button>
+        <button class="bg-slate-400 p-5">7</button>
+        <button class="bg-slate-400 p-5">8</button>
+        <button class="bg-slate-400 p-5">9</button>
+        <button class="bg-orange-400 p-5">+</button>
+        <button class="bg-slate-400 p-5">4</button>
+        <button class="bg-slate-400 p-5">5</button>
+        <button class="bg-slate-400 p-5">6</button>
+        <button class="bg-orange-400 p-5">-</button>
+        <button class="bg-slate-400 p-5">1</button>
+        <button class="bg-slate-400 p-5">2</button>
+        <button class="bg-slate-400 p-5">3</button>
+        <button class="bg-orange-400 p-5">*</button>
+        <button class="bg-slate-400 p-5 col-span-2">0</button>
+        <button class="bg-slate-400 p-5">.</button>
+        <button class="bg-green-400 p-5">=</button> -->
+
+    </div>
+</template>
+
+<script>
+
+    export default {
+        name:'CalculatorApp',
+        props:{
+            msg:String
+        },
+        data(){
+        return{
+            calculatorValue:'',
+            calculatorElements:['C','%','/','-',7,8,9,'+',4,5,6,'*',1,2,3,'=',0,'.'],
+            operator:null,
+            previousCalculatorValue:'',
+        }
+    },
+    methods:{
+        action(n){
+            // append value
+            if(!isNaN(n) || n === '.'){
+                this.calculatorValue += n + '';
+            }
+            // clear value
+            if(n === 'C'){
+                this.calculatorValue = '';
+            }
+            // percentage
+            if(n === '%'){
+                this.calculatorValue = this.calculatorValue / 100 + '';
+            }
+            // 
+            if(['/','*','-','+'].includes(n)){
+                this.operator = n;
+                this.previousCalculatorValue = this.calculatorValue;
+                this.calculatorValue = ''
+            }
+            // 
+            if(n === '='){
+                this.calculatorValue = eval(
+                    this.previousCalculatorValue + this.operator + this.calculatorValue
+                );
+                this.previousCalculatorValue = '';
+                this.operator = null;
+            }
+        }
+    }
+    }
+    
+</script>
+
+<style scoped>
+button ,div{
+    border-radius: 5px;
+}
+button:hover{
+    color:white;
+}
+.bg-vue-green{
+    background:#3fb984;
+}
+div:nth-child(19){
+    grid-column: span 3 / span 3;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <CalculatorApp/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import CalculatorApp from '@/components/CalculatorApp.vue'
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+    CalculatorApp
   }
 }
 </script>
